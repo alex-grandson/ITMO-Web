f65543e1e03ef18e4c638cd10ca4cfcd240201ad
alex-grandson <70079537+alex-grandson@users.noreply.github.com>
2020-09-15 10:27:41 +0300

    Add files via upload

--- Lab1/index.php
+++ Lab1/index.php
@@ -36,15 +36,31 @@
                 </h3>
                 <img src="img/pic1.jpg" alt="picture" class="main-offer__pic">
                 <p class="main-offer__text">
-                  Допустимые значения R: {1, 2, 3, 4, 5}
+                  Выберете R:
+                  <select class="main-offer__select main-offer__select_R" name="r" id="r">
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                  </select>
                 </p>
-                <input class="main-offer__input" type="text" name="r" id="r" placeholder="Введите R" required>
                 <p class="main-offer__message" id="messageR"></p>
 
                 <p class="main-offer__text">
-                  Допустимые значения X: {-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2}
+                  Выберете X:
+                  <select class="main-offer__select main-offer__select_R" name="x" id="x">
+                      <option value="-2">-2</option>
+                      <option value="-1.5">-1.5</option>
+                      <option value="-1">-1</option>
+                      <option value="-0.5">-0.5</option>
+                      <option value="0">0</option>
+                      <option value="0.5">0.5</option>
+                      <option value="1">1</option>
+                      <option value="1.5">1.5</option>
+                      <option value="2">2</option>
+                  </select>
                 </p>
-                <input class="main-offer__input" type="text" name="x" id="x" placeholder="Введите X" required>
                 <p class="main-offer__message" id="messageX"></p>
 
                 <p class="main-offer__text">
@@ -63,25 +79,8 @@
         </div>
     </div>
 </section>
-<!-- Индикация процесса отправки формы -->
-<div id="loader">
-    <img src="img/ripple.svg">
-</div>
-
-<!-- Сообщение "спасибо" после отправки формы -->
-<div id="overlay">
-    <div id="thx">
-        Спасибо! Мы обязательно вам перезвоним
-    </div>
-</div>
-<footer>
-    <div class="container">
-
-    </div>
-</footer>
 
 <script src="js/jquery-3.5.1.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.js"></script>
 <script src="js/main.js" type="text/javascript"></script>
 </body>
 </html>
