d9be51ca95b4c3d63a10f95e9aa946122e9deed1
alex-grandson <70079537+alex-grandson@users.noreply.github.com>
2020-10-13 13:56:05 +0600

    Add files via upload

--- Lab1/save.php
+++ Lab1/save.php
@@ -8,27 +8,37 @@ $validX = array(-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2);
 $r = intval(htmlspecialchars($_GET["r"]));
 $x = floatval(htmlspecialchars($_GET["x"]));
 $y = floatval(htmlspecialchars($_GET["y"]));
+date_default_timezone_set("Europe/Moscow");
 $current_time = date("H:i:s");
 $message = "";
+$class = "No";
+
+if ((($x <= 0 && $y >= 0 && abs($x) <= $r && abs($y) <= $r)) ||
+     ($x >= 0 && $y >= 0 && ($x + $y <= $r)) ||
+     ($x >= 0 && $y <= 0 && 4*($x*$x+$y*$y) <= $r*$r)) {
+  $message = "Yes";
+  $class = "Yes";
+} else {
+  $message = "No";
+}
 
 if (!is_null($r) && !is_null($x) && !is_null($y)) {
-  if (!in_array($r, $validR)) {
-    $message = "Invalid R";
-  }
-  if (!in_array($x, $validX)) {
-    $message = "Invalid X";
-  }
-  if ($y > 3 || $y < -5) {
-    $message = "Invalid Y";
+  if ($r == 0 && $x == 0 && $y == 0) {
+    $message = "Insert Data";
+  } else {
+    if (!in_array($r, $validR)) {
+      $message = "Invalid R";
+    }
+    if (!in_array($x, $validX)) {
+      $message = "Invalid X";
+    }
+    if ($y > 3 || $y < -5) {
+      $message = "Invalid Y";
+    }
   }
 
-  if ((($x <= 0 && $y >= 0 && abs($x) <= $r && abs($y) <= $r)) ||
-       ($x >= 0 && $y >= 0 && ($x + $y <= $r)) ||
-       ($x >= 0 && $y <= 0 && 4*($x*$x+$y*$y) <= $r*$r)) {
-    $message = "Yes";
-  } else {
-    $message = "No";
-  }
+
+
 
   $time = strval(number_format(microtime(true) - $start, 10, ".", "")*1000) . 'ms';
 
@@ -40,8 +50,8 @@ if (!is_null($r) && !is_null($x) && !is_null($y)) {
   array_push($_SESSION['results'], $result);
 
   // Печать в таблицу
-  print_r('<tr><td>'.$x.'</td><td>'.$y.'</td><td>'.$r.'</td><td class="'.$message.'">'.$message.'</td><td>'.$time.'</td><td>'.$current_time.'</td></tr>');
-  
+  print_r('<tr><td>'.$x.'</td><td>'.$y.'</td><td>'.$r.'</td><td class="'.$class.'">'.$message.'</td><td>'.$time.'</td><td>'.$current_time.'</td></tr>');
+  // header('Location: index.php');
 }
 
 
